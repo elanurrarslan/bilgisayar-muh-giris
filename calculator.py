dd1117efe1d89ecbbbd9d33928f790a639752c60
Irem Nur Sebu <16008124071@ogr.bozok.edu.tr>
2025-10-30 11:23:29 +0300

    Başlangıç Mesajı Ekledim

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+print("Hesap Makinesi Projesi Elanur Arslan - İrem Nur Sebü")
+
 def topla(a, b):
     return a + b
 
@@ -10,3 +12,17 @@ def carp(a, b):
 
 if __name__ == "__main__":
     print("Çarpma örnek: 4 * 6 =", carp(4, 6))
+
+def cikar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Çikarma örnek: 10 - 4 =", cikar(10, 4))
+
+
+def bol(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("Bölme örnek: 12 / 3 =", bol(12, 3))
+
